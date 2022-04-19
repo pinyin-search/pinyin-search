76ccf63196079f5f3f369d59e8621862aca40041
jeessy2 <6205259+jeessy2@users.noreply.github.com>
2022-04-19 12:12:50 +0800

    fix: Add index

--- search/meilisearch.go
+++ search/meilisearch.go
@@ -25,19 +25,14 @@ func (meili *MeiliSearch) Init() {
 func (meili *MeiliSearch) Add(tenant string, indexName string, doc []map[string]interface{}) entity.Result {
 	// An index is where the documents are stored.
 	index := meili.Client.Index(tenant + "_" + indexName)
+
 	task, err := index.AddDocuments(doc)
 	if err != nil {
 		log.Printf("添加Index失败。tenant: %s, indexName: %s, err: %s\n", tenant, indexName, err)
 		return entity.Result{Success: false, Msg: err.Error()}
 	}
 
-	t, err := index.GetTask(task.UID)
-	if err != nil {
-		log.Printf("添加Index失败(获取task失败)。tenant: %s, indexName: %s, err: %s", tenant, indexName, err)
-		return entity.Result{Success: false, Msg: err.Error()}
-	}
-	success := t.Status == meilisearch.TaskStatusEnqueued || t.Status == meilisearch.TaskStatusSucceeded || t.Status == meilisearch.TaskStatusProcessing
-	return entity.Result{Success: success, Data: doc}
+	return entity.Result{Success: true, Data: doc, Msg: string(task.Status)}
 }
 
 func (meili *MeiliSearch) Suggestion(tenant string, indexName string, keyword string) entity.Result {
